ecc8bb76999e5eeb2435d1bef86e726795d89a79
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-25 16:34:28 +0530

    ADDED:To valid last name

--- src/main/java/com/bridgelabz/User.java
+++ src/main/java/com/bridgelabz/User.java
@@ -3,6 +3,11 @@ package com.bridgelabz;
 public class User {
     //This method is used to validate the First Name
     public boolean checkFirstName(String firstName) {
-        return (firstName.matches("[A-Z][a-z]{3,}"));
+        return (firstName.matches("^[A-Z][a-z]{3,}$"));
+    }
+
+    //This method is used to validate the Last Name
+    public boolean checkLastName(String lastName) {
+        return (lastName.matches("^[A-Z][a-z]{2,}$"));
     }
 }
--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,13 +1,11 @@
 package com.bridgelabz;
 
-import java.util.regex.Pattern;
-
 public class UserRegistration {
 
     public static void main(String[] args) {
         User userRegistration = new User();
         System.out.println(userRegistration.checkFirstName("Nilesh"));
-
+        System.out.println(userRegistration.checkLastName("Lad"));
        }
     }
 
