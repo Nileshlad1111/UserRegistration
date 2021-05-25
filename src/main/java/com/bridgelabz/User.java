e7e58d0ee6f260e2be945585602d6bf043fbf30e
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-25 17:28:01 +0530

    MODIFIED:To valid passwaord rule-3

--- src/main/java/com/bridgelabz/User.java
+++ src/main/java/com/bridgelabz/User.java
@@ -23,6 +23,6 @@ public class User {
 
     //this method is used to validate password
     public boolean checkPassword(String password) {
-        return (password.matches("^(?=.*[A-Z])(?=.*[a-z]).{8,}$"));
+        return (password.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$"));
     }
 }
