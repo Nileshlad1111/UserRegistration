4e91c7d9a5f528049123399b17f626fe2cd06b57
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-25 16:44:19 +0530

    ADDED:To valid Email

--- src/main/java/com/bridgelabz/User.java
+++ src/main/java/com/bridgelabz/User.java
@@ -10,4 +10,10 @@ public class User {
     public boolean checkLastName(String lastName) {
         return (lastName.matches("^[A-Z][a-z]{2,}$"));
     }
+
+    //  This method is used to validate email Id
+    public boolean checkEmail(String email){
+        return (email.matches("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$"));
+    }
+
 }
--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -3,9 +3,12 @@ package com.bridgelabz;
 public class UserRegistration {
 
     public static void main(String[] args) {
-        User userRegistration = new User();
-        System.out.println(userRegistration.checkFirstName("Nilesh"));
-        System.out.println(userRegistration.checkLastName("Lad"));
-       }
+        //object
+        User user = new User();
+        //method call
+        System.out.println(user.checkFirstName("Nilesh"));
+        System.out.println(user.checkLastName("Lad"));
+        System.out.println(user.checkEmail("ladnilesh1994@gmail.com"));
     }
+   }
 
